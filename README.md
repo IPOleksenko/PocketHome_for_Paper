e1d3948aa514e0226feb6614b67624c71b2b15e5
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-01-27 02:44:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Paper plugin for pocket dimensions
+
+## Authors
+  - [IPOleksenko](https://github.com/IPOleksenko) (owner)
+  - [rvbsm](https://github.com/rvbsm)
+
---
+++ src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
@@ -0,0 +1,11 @@
+package dev.ipoleksenko.PocketHome;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PocketHomePlugin extends JavaPlugin {
+  
+  @Override
+  public void onEnable() {
+    
+  }
+}
