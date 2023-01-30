723341c1d41ef7cf3e7a32fe3c610e5b7ef0fdd7
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-01-30 06:37:36 +0200

    update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("java")
 }
 
-group = "dev.ipoleksenko"
-version = "1.0-SNAPSHOT"
+group = project.properties["maven_group"].toString()
+version = project.properties["plugin_version"].toString()+getVersionMetadata()
 
 repositories {
     mavenCentral()
@@ -20,10 +20,18 @@ java {
 
 tasks {
     processResources {
-        val props = linkedMapOf("version" to version, "api_version" to project.properties["paper_api_version"])
+        val props = linkedMapOf("version" to version, "api_ version" to project.properties["paper_api_version"])
         inputs.properties(props)
         filesMatching("plugin.yml") {
             expand(props)
         }
     }
+}
+
+fun getVersionMetadata(): String {
+    val buildId = System.getenv("GITHUB_RUN_NUMBER")
+
+    if (buildId != null)
+        return "+build.${buildId}"
+    return ""
 }
