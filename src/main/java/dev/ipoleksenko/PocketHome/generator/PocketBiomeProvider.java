c2bb037f55cfaf0d9a233c39671c30d4d9601eca
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-01-31 07:03:31 +0200

    feat: pocket world

---
+++ src/main/java/dev/ipoleksenko/PocketHome/generator/PocketBiomeProvider.java
@@ -0,0 +1,20 @@
+package dev.ipoleksenko.PocketHome.generator;
+
+import org.bukkit.block.Biome;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.WorldInfo;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PocketBiomeProvider extends BiomeProvider {
+    @Override
+    public @NotNull Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
+        return Biome.THE_VOID;
+    }
+
+    @Override
+    public @NotNull List<Biome> getBiomes(@NotNull WorldInfo worldInfo) {
+        return List.of(Biome.THE_VOID);
+    }
+}
---
+++ src/main/java/dev/ipoleksenko/PocketHome/generator/PocketChunkGenerator.java
@@ -0,0 +1,54 @@
+package dev.ipoleksenko.PocketHome.generator;
+
+import org.bukkit.World;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PocketChunkGenerator extends ChunkGenerator {
+
+    @Override
+    public @NotNull List<BlockPopulator> getDefaultPopulators(@NotNull World world) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public BiomeProvider getDefaultBiomeProvider(@NotNull WorldInfo worldInfo) {
+        return new PocketBiomeProvider();
+    }
+
+    @Override
+    public boolean shouldGenerateNoise() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateSurface() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateCaves() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateDecorations() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateMobs() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateStructures() {
+        return false;
+    }
+}
