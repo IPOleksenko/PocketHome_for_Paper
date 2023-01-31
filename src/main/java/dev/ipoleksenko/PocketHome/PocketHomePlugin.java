65419a71303fec865b245ebcb16689062ed3b304
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-01-31 08:51:02 +0200

    feat: add getInstance()

--- src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
+++ src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
@@ -3,9 +3,18 @@ package dev.ipoleksenko.PocketHome;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PocketHomePlugin extends JavaPlugin {
+  public static PocketHomePlugin instance;
+
+  public static PocketHomePlugin getInstance() {
+    return instance;
+  }
   
   @Override
   public void onEnable() {
-    
+    instance = this;
+  }
+
+  @Override
+  public void onDisable() {
   }
 }
