9e139c2cd58fb388eb63c4168520a1bf0f1fd0f7
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-02-02 22:28:49 +0200

    feat: add methods for player teleport

--- src/main/java/dev/ipoleksenko/PocketHome/manager/PocketManager.java
+++ src/main/java/dev/ipoleksenko/PocketHome/manager/PocketManager.java
@@ -2,6 +2,7 @@ package dev.ipoleksenko.PocketHome.manager;
 
 import dev.ipoleksenko.PocketHome.generator.PocketChunkGenerator;
 import org.bukkit.*;
+import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
 
 import javax.annotation.Nullable;
@@ -28,6 +29,48 @@ public class PocketManager {
 
 	private ChunkGenerator getGenerator() {
 		return this.generator;
+	}
+
+
+	/**
+	 * Teleports player to his pocket
+	 *
+	 * @param player player instance
+	 * @return true if success, false otherwise
+	 */
+	public boolean teleportToPocket(Player player) {
+		return this.teleportToPocket(player, player.getName());
+	}
+
+	/**
+	 * Teleports player to a specified pocket
+	 *
+	 * @param player     player instance
+	 * @param pocketName name of a pocket
+	 * @return true if success, false otherwise
+	 */
+	public boolean teleportToPocket(Player player, String pocketName) {
+		World pocket = this.getPocket(pocketName, player.getName().equals(pocketName));
+		if (pocket == null) return false;
+
+		player.teleport(pocket.getSpawnLocation());
+		player.setInvulnerable(true);
+		player.setAllowFlight(true);
+		return true;
+	}
+
+	/**
+	 * Teleports player from a pocket to overworld
+	 *
+	 * @param player player instance
+	 */
+	public void teleportFromPocket(Player player) {
+		World world = Bukkit.getWorlds().get(0);
+
+		player.teleport(world.getSpawnLocation());
+		player.setInvulnerable(false);
+		player.setAllowFlight(false);
+		player.setFlying(false);
 	}
 
 
