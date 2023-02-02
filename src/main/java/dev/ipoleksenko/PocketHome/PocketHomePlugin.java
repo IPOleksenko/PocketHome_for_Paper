8214711ccf1b55557be76acb86217e2d2f416777
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-02-02 06:56:45 +0200

    style: change indentation from spaces to tab

--- src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
+++ src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
@@ -5,25 +5,25 @@ import dev.ipoleksenko.PocketHome.manager.PocketManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PocketHomePlugin extends JavaPlugin {
-    public static PocketHomePlugin instance;
-    private PocketManager pocketManager;
+	public static PocketHomePlugin instance;
+	private PocketManager pocketManager;
 
-    public static PocketHomePlugin getInstance() {
-        return instance;
-    }
+	public static PocketHomePlugin getInstance() {
+		return instance;
+	}
 
-    public PocketManager getWorldManager() {
-        return this.pocketManager;
-    }
+	public PocketManager getWorldManager() {
+		return this.pocketManager;
+	}
 
-    @Override
-    public void onEnable() {
-        instance = this;
+	@Override
+	public void onEnable() {
+		instance = this;
 
-        pocketManager = new PocketManager(new PocketChunkGenerator());
-    }
+		pocketManager = new PocketManager(new PocketChunkGenerator());
+	}
 
-    @Override
-    public void onDisable() {
-    }
+	@Override
+	public void onDisable() {
+	}
 }
