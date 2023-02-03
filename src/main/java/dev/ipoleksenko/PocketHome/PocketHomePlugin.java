0dee7117d4bdbf4996a32c2f2dd19c59e182203d
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-02-03 05:35:44 +0200

    feat: load pocket worlds at launch

--- src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
+++ src/main/java/dev/ipoleksenko/PocketHome/PocketHomePlugin.java
@@ -2,7 +2,11 @@ package dev.ipoleksenko.PocketHome;
 
 import dev.ipoleksenko.PocketHome.generator.PocketChunkGenerator;
 import dev.ipoleksenko.PocketHome.manager.PocketManager;
+import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class PocketHomePlugin extends JavaPlugin {
 	public static PocketHomePlugin instance;
@@ -20,6 +24,23 @@ public class PocketHomePlugin extends JavaPlugin {
 	public void onEnable() {
 		instance = this;
 		pocketManager = new PocketManager(new PocketChunkGenerator());
+
+		loadPocketWorlds();
+	}
+
+	private void loadPocketWorlds() {
+		File dir = new File("./pockets/");
+		if (!dir.exists()) return;
+
+		File[] dirPockets = dir.listFiles();
+		if (dirPockets == null) return;
+		
+		for (File file : dirPockets)
+			if (file.isDirectory()) {
+				String[] pocketFiles = file.list();
+				if (pocketFiles != null && Arrays.asList(pocketFiles).contains("level.dat"))
+					WorldCreator.name("pockets/" + file.getName()).createWorld();
+			}
 	}
 
 	@Override
