8f5ecf4f3758d6242cfec33d0ed50acaa074c930
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-02-06 15:05:03 +0200

    refactor: save UUID as an int array

--- src/main/java/dev/ipoleksenko/PocketHome/util/UUIDDataType.java
+++ src/main/java/dev/ipoleksenko/PocketHome/util/UUIDDataType.java
@@ -2,17 +2,38 @@ package dev.ipoleksenko.PocketHome.util;
 
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.ByteBuffer;
 import java.util.UUID;
 
-public class UUIDDataType implements PersistentDataType<byte[], UUID> {
+public class UUIDDataType implements PersistentDataType<int[], UUID> {
+
+	public static int @NotNull [] toInts(@NotNull UUID uuid) {
+		int[] ints = new int[4];
+		long mostSigBits = uuid.getMostSignificantBits();
+		long leastSigBits = uuid.getLeastSignificantBits();
+
+		ints[0] = (int) (mostSigBits >> 32);
+		ints[1] = (int) mostSigBits;
+		ints[2] = (int) (leastSigBits >> 32);
+		ints[3] = (int) leastSigBits;
+
+		return ints;
+	}
+
+	@Contract(value = "_ -> new", pure = true)
+	public static @NotNull UUID fromInts(int @NotNull [] ints) {
+		long mostSigBits = (long) ints[0] << 32 | ints[1] & 0xFFFFFFFFL;
+		long leastSigBits = (long) ints[2] << 32 | ints[3] & 0xFFFFFFFFL;
+
+		return new UUID(mostSigBits, leastSigBits);
+	}
 
 	@Override
 	@NotNull
-	public Class<byte[]> getPrimitiveType() {
-		return byte[].class;
+	public Class<int[]> getPrimitiveType() {
+		return int[].class;
 	}
 
 	@Override
@@ -22,19 +43,13 @@ public class UUIDDataType implements PersistentDataType<byte[], UUID> {
 	}
 
 	@Override
-	public byte @NotNull [] toPrimitive(@NotNull UUID complex, @NotNull PersistentDataAdapterContext context) {
-		ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
-		bb.putLong(complex.getMostSignificantBits());
-		bb.putLong(complex.getLeastSignificantBits());
-		return bb.array();
+	public int @NotNull [] toPrimitive(@NotNull UUID complex, @NotNull PersistentDataAdapterContext context) {
+		return toInts(complex);
 	}
 
 	@Override
 	@NotNull
-	public UUID fromPrimitive(byte @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
-		ByteBuffer bb = ByteBuffer.wrap(primitive);
-		long mostSigBits = bb.getLong();
-		long leastSigBits = bb.getLong();
-		return new UUID(mostSigBits, leastSigBits);
+	public UUID fromPrimitive(int @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
+		return fromInts(primitive);
 	}
 }
--- src/main/java/dev/ipoleksenko/PocketHome/util/UUIDListDataType.java
+++ src/main/java/dev/ipoleksenko/PocketHome/util/UUIDListDataType.java
@@ -4,16 +4,15 @@ import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-public class UUIDListDataType implements PersistentDataType<byte[], List> {
+public class UUIDListDataType implements PersistentDataType<int[][], List> {
 	@Override
 	@NotNull
-	public Class<byte[]> getPrimitiveType() {
-		return byte[].class;
+	public Class<int[][]> getPrimitiveType() {
+		return int[][].class;
 	}
 
 	@Override
@@ -23,29 +22,19 @@ public class UUIDListDataType implements PersistentDataType<byte[], List> {
 	}
 
 	@Override
-	public byte @NotNull [] toPrimitive(@NotNull List complex, @NotNull PersistentDataAdapterContext context) {
-		ByteBuffer bb = ByteBuffer.wrap(new byte[complex.size() * 16]);
+	public int @NotNull [][] toPrimitive(@NotNull List complex, @NotNull PersistentDataAdapterContext context) {
+		int[][] primitive = new int[complex.size()][4];
+		List<int[]> ints = ((List<UUID>) complex).stream().map(UUIDDataType::toInts).toList();
 
-		for (UUID uuid : (List<UUID>) complex) {
-			bb.putLong(uuid.getMostSignificantBits());
-			bb.putLong(uuid.getLeastSignificantBits());
-		}
+		for (int i = 0; i < ints.size(); ++i)
+			primitive[i] = ints.get(i);
 
-		return bb.array();
+		return primitive;
 	}
 
 	@Override
 	@NotNull
-	public List<UUID> fromPrimitive(byte @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
-		ByteBuffer bb = ByteBuffer.wrap(primitive);
-		List<UUID> uuids = new ArrayList<UUID>();
-
-		for (int i = 0; i < primitive.length / 16; ++i) {
-			long mostSigBits = bb.getLong();
-			long leastSigBits = bb.getLong();
-			uuids.add(new UUID(mostSigBits, leastSigBits));
-		}
-
-		return uuids;
+	public List<UUID> fromPrimitive(int @NotNull [][] primitive, @NotNull PersistentDataAdapterContext context) {
+		return Arrays.stream(primitive).map(UUIDDataType::fromInts).toList();
 	}
 }
